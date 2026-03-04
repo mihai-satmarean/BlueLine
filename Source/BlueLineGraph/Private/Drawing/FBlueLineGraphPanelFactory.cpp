6a61d48f738152b9b85f7564e93f9ba95edfa5cc
Andras Gregori <andgregorik@gmail.com>
2026-03-04 18:38:49 +0100

    Delete Source directory

--- Source/BlueLineGraph/Private/Drawing/FBlueLineGraphPanelFactory.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) 2026 GregOrigin. All Rights Reserved.
-
-#include "Drawing/FBlueLineGraphPanelFactory.h"
-
-// Factory implementation is intentionally empty.
-// Connection Drawing hooks must now be implemented via UEdGraphSchema::CreateConnectionDrawingPolicy
-// within a custom Schema class.
